86648aa94bc03de93a53cb5742918905fcc1d3b5
Carlos E. Carvalho <carlosalvc@gmail.com>
2015-10-20 14:32:54 +0100

    Initial commit

---
+++ commands/EditCmd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#change editor preferences below
+EDITOR="Visual Studio Code"
+
+echo "Go code them master!"
+
+open -a $EDITOR $1
---
+++ init.sh
@@ -0,0 +1,12 @@
+#general alias
+source ~/.dotfiles/commands/aliases
+
+#caffe python env
+#source ~/.dotfiles/env/caffe
+
+#java setup
+source ~/.dotfiles/env/java
+
+#maven setup
+
+source ~/.dotfiles/env/maven
